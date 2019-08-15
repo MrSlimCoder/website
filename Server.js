fe7913ef2caf70e24577aa77b93e7952ddbcbe97
Riya <kato.hana@outlook.jp>
2019-08-15 18:33:52 +0200

    Added a few basic example routes for POC

--- Server.js
+++ Server.js
@@ -16,6 +16,9 @@ class Server {
         const nextHandler = app.getRequestHandler();
 
         app.prepare().then(() => {
+            this.server.get('/about', (req, res) => app.render(req, res, '/about', req.query));
+            this.server.get('/:username', (req, res) => app.render(req, res, '/profile', { username: req.params.username }));
+            this.server.get('/', (req, res) => app.render(req, res, '/landing', req.query));
             this.server.get('*', nextHandler);
             this.server.listen(this.port, (error) => {
                 if (error) throw error;
