28d1ab7a6c2e12fbc3d25dbcf4a6df04bec8506e
Jonah Snider <jonah@jonah.pw>
2020-02-06 19:17:17 -0800

    refactor(next): remove unused argument to `webpack()`
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
     useFileSystemPublicRoutes: false,
-    webpack(config, options) {
+    webpack(config) {
         config.module.rules.push({
             test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
             use: {
